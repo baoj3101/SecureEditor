8df89b09bd004ab0e64f3a8ec22e06e17144ecf5
xbao <xbao@XBAO3.na.qualcomm.com>
2019-05-22 15:48:11 -0700

    2nd draft

--- src/BaseEditor.java
+++ src/BaseEditor.java
@@ -1,5 +1,8 @@
 /**
- * BaseEditor Class: very basic text editor supporting copy/paste
+ * BaseEditor Class
+ *   Very basic text editor based on SWING which supports 
+ *   copy (Ctrl+C), cut (Ctrl+X) and paste (Ctrl+V)
+ *   
  * @author baoj3101
  */
 
@@ -8,31 +11,30 @@ import javax.swing.*;
 import javax.swing.text.*;
 
 public class BaseEditor {
-    // editor frame and text pane
-    protected JFrame    frame;
-    protected JTextPane textPane;
-    protected JScrollPane editorScrollPane;
+    // editor frame, text pane, and scroll pane
+    protected JFrame      frame;
+    protected JTextPane   textPane;
+    protected JScrollPane scrollPane;
+    
     // constructor
     public BaseEditor () {
         // initialize frame
         frame = new JFrame("Base Editor");
-        frame.setSize(800, 800);
-        frame.setLocation(100, 100);
+        frame.setSize(800, 800);          // window size
+        frame.setLocation(100, 100);      // window location
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // initialize text pane
         textPane = new JTextPane();
         textPane.setDocument(new DefaultStyledDocument());
-        editorScrollPane = new JScrollPane(textPane);
         
-        frame.add(editorScrollPane, BorderLayout.CENTER);
+        // initialize scroll pane
+        scrollPane = new JScrollPane(textPane);
+        frame.add(scrollPane, BorderLayout.CENTER);
     }
 
     // show
     public void show() {
-        // add text pane to frame and show
-        JScrollPane scrollPane = new JScrollPane(textPane);
-        frame.add(scrollPane, BorderLayout.CENTER);
         frame.setVisible(true);
         textPane.requestFocusInWindow();        
     }
@@ -42,7 +44,7 @@ public class BaseEditor {
 	return (DefaultStyledDocument) textPane.getDocument();
     }
     
-    // main for testing
+    // main method for testing
     public static void main(String [] args) {
         BaseEditor e = new BaseEditor();
         e.show();
